0ebd9d519d04c09276996b514ccc9e596ebf4e9d
jef <324976+xbrln@users.noreply.github.com>
2021-08-28 14:13:36 +0200

    Add clojars group id to projects.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject days-on-earth "0.1.0"
+(defproject org.clojars.xbrln/days-on-earth "0.1.0"
   :description "A Clojure library designed to calculate the number of days you have lived on earth!"
   :url "https://github.com/xbrln/days-on-earth"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
